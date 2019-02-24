3c53bccbeada81ef5eb2377526fcd29a8d48a284
Yahya Sahaja <yahya.sahaja2@gmail.com>
2019-02-24 07:20:33 +0700

    deployment

--- models/connection.js
+++ models/connection.js
@@ -1,11 +1,14 @@
 const Sequalize = require('sequelize');
+const test = process.env.NODE_ENV === 'development'
+console.log(process.env.NODE_ENV)
+const password = test ? 'yahya123' : 'sansaja123'
 
-const conn = new Sequalize('instaphoto', 'root', 'yahya123', {
+const conn = new Sequalize('instaphoto', 'root', password, {
     dialect: 'mysql',
     host: 'localhost',
     port: 3306,
     logging: false,
     operatorsAliases: false
-});
+})
 
 module.exports = conn
